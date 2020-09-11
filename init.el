f80fe65333689dd09de475a2341d9449f356803e
Ben Brodie <bbrodie@caisgroup.com>
2020-09-11 12:27:16 -0700

    add org capture templates

--- init.el
+++ init.el
@@ -98,10 +98,17 @@
   :config
   (evil-collection-init))
 
-;; (use-package evil-org
-;;   :ensure t
-;;   :after evil
-;;   :hook (org-mode . evil-org-mode))
+(use-package evil-org
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda ()
+              (evil-org-set-key-theme
+               '(textobjects insert navigation additional shift todo))))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
 
 (use-package evil-magit
   :after (evil magit)
@@ -237,9 +244,25 @@
 
 ;; org-mode
 (require 'org)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cc" 'org-capture)
+(setq org-default-notes-file "~/Documents/gtd/inbox.org")
 (setq org-agenda-files "~/.emacs.d/org-agenda-files")
+(setq org-todo-keywords
+      '((sequence "TODO" "IN-PROGRESS" "DONE")))
+
+(require 'org-capture)
+(global-set-key "\C-cc" 'org-capture)
+(setq org-capture-templates '(("t" "TODO" entry (file "~/Documents/gtd/inbox.org")
+                               "* TODO %?\n %U\n %a")
+                              ("h" "Habit" entry (file "~/Documents/gtd/habits.org")
+                               "* TODO %?\n  SCHEDULED: %t\n  :PROPERTIES:\n  :STYLE: habit\n  :END:")))
+
+(require 'org-clock)
+(setq org-clock-idle-time 10)
+(require 'org-agenda)
+(global-set-key "\C-ca" 'org-agenda)
+(add-to-list 'org-modules 'org-habit)
+(setq org-agenda-window-setup 'current-window)
+(setq org-agenda-todo-ignore-scheduled 'future)
 
 (use-package org-present
   :ensure t
@@ -435,9 +458,12 @@
  '(nrepl-message-colors
    (quote
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(org-agenda-files
+   (quote
+    ("~/Documents/gtd/habits.org" "~/Documents/gtd/inbox.org" "~/Documents/cais.org")))
  '(package-selected-packages
    (quote
-    (column-marker evil-matchit browse-kill-ring java-imports zoom-window dumb-jump gtags groovy-mode ripgrep web-mode yari ctags-update spaceline wget evil-collection wgrep-ag use-package string-inflection json-mode evil-surround rg counsel-projectile evil-magit rjsx-mode js2-mode hide-mode-line org-present yaml-mode ivy-hydra hydra counsel ivy rubocop haskell-mode ws-butler markdown-mode alchemist ag ace-window zenburn-theme evil-snipe column-enforce-mode flx-ido company yasnippet yasnippet-snippets meghanada projectile flycheck exec-path-from-shell restclient erlang evil)))
+    (column-marker evil-matchit browse-kill-ring java-imports zoom-window dumb-jump gtags groovy-mode ripgrep web-mode yari ctags-update spaceline wget evil-collection wgrep-ag use-package string-inflection json-mode evil-surround rg counsel-projectile evil-magit rjsx-mode js2-mode hide-mode-line org-present yaml-mode evil-org ivy-hydra hydra counsel ivy rubocop haskell-mode ws-butler markdown-mode alchemist ag ace-window zenburn-theme evil-snipe column-enforce-mode flx-ido company yasnippet yasnippet-snippets meghanada projectile flycheck exec-path-from-shell restclient erlang evil)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(safe-local-variable-values (quote ((column-enforce-column . 120))))
  '(tool-bar-mode nil)
