347ae0656744f9a11b76d0f009722c2b9728e651
Ben Brodie <bbrodie@caisgroup.com>
2020-09-15 16:56:28 -0700

    add airmail linking

--- init.el
+++ init.el
@@ -254,6 +254,16 @@
 (setq org-refile-targets '((org-agenda-files :maxlevel . 1)))
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "DONE")))
+(org-add-link-type "airmail" 'org-airmail-open)
+
+(defun org-airmail-open (url)
+  "Visit the Airmail message referenced by URL.
+URL should be a vaid Airmail message url retrieved from Airmail with
+'Copy Message Link'."
+  (shell-command
+   ;; Note: org strips "airmail:" from the link URL
+   (concat "open -a '/Applications/Airmail.app' airmail:"
+           (shell-quote-argument url))))
 
 (require 'org-capture)
 (global-set-key "\C-cc" 'org-capture)
@@ -473,7 +483,7 @@
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (evil-org column-marker evil-matchit browse-kill-ring java-imports zoom-window dumb-jump gtags groovy-mode ripgrep web-mode yari ctags-update spaceline wget evil-collection wgrep-ag use-package string-inflection json-mode evil-surround rg counsel-projectile evil-magit rjsx-mode js2-mode hide-mode-line org-present yaml-mode ivy-hydra hydra counsel ivy rubocop haskell-mode ws-butler markdown-mode alchemist ag ace-window zenburn-theme evil-snipe column-enforce-mode flx-ido company yasnippet yasnippet-snippets meghanada projectile flycheck exec-path-from-shell restclient erlang evil)))
+    (column-marker evil-matchit browse-kill-ring java-imports zoom-window dumb-jump gtags groovy-mode ripgrep web-mode yari ctags-update spaceline wget evil-collection wgrep-ag use-package string-inflection json-mode evil-surround rg counsel-projectile evil-magit rjsx-mode js2-mode hide-mode-line org-present yaml-mode evil-org ivy-hydra hydra counsel ivy rubocop haskell-mode ws-butler markdown-mode alchemist ag ace-window zenburn-theme evil-snipe column-enforce-mode flx-ido company yasnippet yasnippet-snippets meghanada projectile flycheck exec-path-from-shell restclient erlang evil)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(safe-local-variable-values (quote ((column-enforce-column . 120))))
  '(tool-bar-mode nil)
